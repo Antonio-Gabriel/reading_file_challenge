9c41cbb6fc5d8eabd6f2baac905e00df0c923f5a
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-05-30 01:31:45 +0100

    save bulk data in the database

---
+++ system/SaveBulkDataFileToDatabase.py
@@ -0,0 +1,45 @@
+from typing import Type
+
+from tabulate import tabulate
+from utils.text_conversor import text_conversor
+
+from mapper.MappingDataFromFile import MappingDataFromFile
+from infra.repository.GenericRepository import GenericRepository
+
+class SaveBulkDataFileToDatabase:
+    def __init__(self, table_name: str, mapper: Type[MappingDataFromFile]) -> None:
+        self.__mapper = mapper
+        self.__generic_repo = GenericRepository(table_name)
+
+    def save_bulk_data_attr(self) -> list:
+        """get headers from list and create dynamic attributes to database"""
+        
+        attrs = (
+            self.__mapper
+            .extract_content_to_file_and_add_to_array()
+            )        
+
+        headers = []
+        for attr in attrs[0]:
+            # strip text and convert to lower
+            attr_converted = text_conversor(attr)
+            headers.append(attr_converted)
+        
+        # delete headers
+        del attrs[0]
+
+        # debug table preview
+        custom_tabulate = tabulate(attrs, headers=headers)
+        print(custom_tabulate)     
+                
+        # # save bulk
+        affected_rows = self.__generic_repo.save_bulk(
+            headers,
+            attrs
+        )
+      
+        return {
+            "affected_rows": affected_rows,
+            "total_records_from_file": len(attrs),
+            "data": attrs,
+        }
