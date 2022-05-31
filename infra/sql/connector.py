32ddbaec8e0beee439081fe086603012c4ffde7c
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-05-31 13:00:24 +0100

    add docker credentials for mysql database

--- infra/sql/connector.py
+++ infra/sql/connector.py
@@ -1,7 +1,7 @@
 from mysql import connector
 
 conn = connector.connect(
-    user="root", password="", host="localhost", database="recicla"
+    user="root", password="12345", host="db", database="recicla"
 )
 
 cursor = conn.cursor()
