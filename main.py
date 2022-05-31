9fa306f5db27d14701c90c97487fe2293fd071aa
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-05-31 14:14:49 +0100

    load env

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import unittest
 
+from dotenv import load_dotenv
+
 from uvicorn import run
 from fastapi import FastAPI, Form, Request, File, UploadFile
 
@@ -8,6 +10,8 @@ from fastapi.templating import Jinja2Templates
 from mapper.MappingDataFromFile import MappingDataFromFile
 from system.WriteDataFromLocalFile import WriteDataFromLocalFile
 from system.SaveBulkDataFileToDatabase import SaveBulkDataFileToDatabase
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -18,15 +22,11 @@ templates = Jinja2Templates(directory="views")
 def index(request: Request):
     """Show the index page to upload file"""
 
-    return templates.TemplateResponse(
-        "index.html", {"request": request, "username": "Atónio Gabriel"}
-    )
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.post("/upload-file")
-async def handle_form(
-    request: Request, tablename: str = Form(...), file: UploadFile = File(...)
-):
+async def handle_form(tablename: str = Form(...), file: UploadFile = File(...)):
     """endpoint for reading file data"""
 
     content_file = await file.read()
@@ -46,4 +46,4 @@ async def handle_form(
 
 if __name__ == "__main__":
     run(app, host="127.0.0.1", port=8080)
-    # unittest.main()
+# unittest.main()
